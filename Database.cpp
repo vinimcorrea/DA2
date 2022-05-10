dbfca4a4d48bbcd9956250136bca08576865a1af
Eduardo Duarte <duarte.eduardoeds@gmail.com>
2022-05-10 14:58:31 +0100

    Inicial commitsing

---
+++ Database.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Vinícius Correa on 10/05/2022.
+//
+
+#include "Database.h"
---
+++ Database.h
@@ -0,0 +1,14 @@
+//
+// Created by Vinícius Correa on 10/05/2022.
+//
+
+#ifndef SECOND_PROJECT_DATABASE_H
+#define SECOND_PROJECT_DATABASE_H
+
+
+class Database {
+
+};
+
+
+#endif //SECOND_PROJECT_DATABASE_H
